54aa81b713e80641be8a0d7b3e9d7e949cf5d8d2
Lauryn <laurynpearson@gmail.com>
2019-06-25 19:29:12 -0500

    add data to database using console and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "Fresh Laundry", price: 3, image_url: "https://images-na.ssl-images-amazon.com/images/I/31oiNKI4GTL._SX355_.jpg", description: "Have the scent of fresh laundry around all the time!")
+product.save
