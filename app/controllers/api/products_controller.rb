c7df2e9a221b3a354716ed072abf3455cb4a41ff
Lauryn <laurynpearson@gmail.com>
2019-06-30 09:28:31 -0500

    restful index action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,19 +1,24 @@
 class Api::ProductsController < ApplicationController
-  def all_available_products
-    @product = Product.first
-    render 'available_products.json.jb'
-  end
+  # def all_available_products
+  #   @product = Product.first
+  #   render 'available_products.json.jb'
+  # end
 
-  def find_by
-    product_name = params["name"]
-    @product = Product.find_by(name: product_name)
-    p @product
-    render 'available_products.json.jb'
-  end
+  # def find_by
+  #   product_name = params["name"]
+  #   @product = Product.find_by(name: product_name)
+  #   p @product
+  #   render 'available_products.json.jb'
+  # end
 
-  def all_products
+  # def all_products
+  #   @products = Product.all
+  #   render 'all.json.jb'
+  # end
+
+  def index
     @products = Product.all
-    render 'all.json.jb'
+    render 'index.json.jb'
   end
 
 end
